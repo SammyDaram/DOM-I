12a4c2d4cc085586e9b01675333c5452c0cb3fb8
SammyDaram <91257206+SammyDaram@users.noreply.github.com>
2022-03-08 18:50:50 -0500

    on track

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const siteContent = { // DO NOT CHANGE THIS OBJECT
   "nav": {
-    "nav-item-1": "Services",
+    "nav-item-1": "Services", 
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
     "nav-item-4": "Features",
@@ -38,5 +38,38 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "accent-img": "http://localhost:9000/img/accent.png",
   },
 };
+//IMAGES
+const logoImg = document.querySelector("#logo-img")
+logoImg.src = siteContent.images["logo-img"]
 
-console.log('project wired!')
+const ctaImg = document.querySelector("#cta-img")
+ctaImg.src = siteContent.images["cta-img"]
+
+const midImg = document.querySelector("#middle-img")
+midImg.setAttribute("src", siteContent.images["accent-img"])
+
+
+//FOOTER LINK 
+const footerLink = document.querySelector("footer a")
+footerLink.textContent = siteContent.footer.copyright
+footerLink.classList.add("bold")
+
+//CONTACT
+const contact = document.querySelector("section.contact")
+contact.querySelector("h4").textContent = siteContent.contact["contact-h4"]
+contact.children[1].textContent = siteContent.contact["address"]
+contact.querySelector("p:nth-of-type(2)").textContent = siteContent.contact["phone"]
+contact.children[3].textContent = siteContent.contact["email"]
+//3 different approaches
+
+
+//TOP CONTENT 
+const topContent = document.querySelector(".top-content")
+topContent.children[0].children[0].textContent = siteContent["main-content"]["features-h4"]
+topContent.children[0].children[1].textContent = siteContent["main-content"]["features-content"]
+topContent.children[1].children[0].textContent = siteContent["main-content"]["about-h4"]
+topContent.children[1].children[1].textContent = siteContent["main-content"]["about-content"]
+
+
+//BOTTOM CONTENT
+const bottomContent = document.querySelector(".bottom-content")
